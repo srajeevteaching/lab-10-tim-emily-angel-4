9bcdef1e72770e994c8ef09d21dcc70c34248a8e
angel scott <ascott1@loyola.edu>
2021-12-02 12:04:31 -0500

    lab 10

--- lab10.py
+++ lab10.py
@@ -7,18 +7,32 @@
 
 ENGLISH = 0
 MORSE = 1
+
 def load_morse_dictionary():
     try:
         morse_code_file = open("morsecode.txt", "r")
         morse_codes = []
         for line in morse_code_file:
-            code = line.split("\t").split("\n")
-            #code[ENGLISH] = str(code[ENGLISH])
-            #code[MORSE] = str(code[MORSE])
+            code = line.strip().split("\t")
             morse_codes.append(code)
         morse_code_file.close()
+        morse_code_dictionary = {}
+        for i in morse_codes:
+            morse_code_dictionary[i[MORSE]] = i[ENGLISH]
     except FileNotFoundError:
         print("Error. File not found.")
-    return morse_codes
+    return morse_code_dictionary
 
-print(load_morse_dictionary())
+def decode_file(file_name, dictionary):
+    try:
+        new_file = open(file_name, "r")
+        new_file_morse = []
+        for line in new_file:
+            line = line.strip().split()
+            new_file_morse.append(line)
+        new_file.close()
+        for i in new_file_morse:
+            if i in dictionary:
+                print(dictionary[i])
+    except FileNotFoundError:
+        print("Error. File not found.")
